9ca5b1f77ef7963d3f01b3cd0e545fd95d49d4d5
Angel Amélia Halouane <63232950+angeleads@users.noreply.github.com>
2023-02-12 18:50:30 +0100

    [ADD] Base of the swiping cards is done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,13 +2,44 @@ import React from 'react'
 import { useNavigation } from "@react-navigation/core"
 import { Button, Text, View, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons"
+import Swiper from "react-native-deck-swiper";
+
+const DUMMY_DATA = [
+  {
+    firstName: "Theo",
+    lastName: "Pawelec",
+    occupation: "Software Developer",
+    photoURL: "https://media.licdn.com/dms/image/C4D03AQETd0DNFHIudg/profile-displayphoto-shrink_200_200/0/1596456033039?e=1681344000&v=beta&t=OiRh28DvRlu9LGt_tB6RdAmdFoDpzXn8F4bDqD5nstY",
+    interests: "I love playing LOL and I can't help but be the best APE ever made",
+    age: 27,
+    id: 1,
+  },
+  {
+    firstName: "Rosa",
+    lastName: "Barchein",
+    occupation: "Bitch idk at this point",
+    photoURL: "https://www.epitech-it.es/wp-content/uploads/2022/11/Rosa-Barchein-BW.jpg",
+    interests: "I hate my life so I have to make everyone else's worse than mine",
+    age: 86,
+    id: 2,
+  },
+  {
+    firstName: "Corentin",
+    lastName: "Coutret-Rozet",
+    occupation: "The queen herself",
+    photoURL: "https://i.pinimg.com/280x280_RS/78/f1/2a/78f12adca94da4621638d8a9736fed56.jpg",
+    interests: "Gay and slay",
+    age: 22,
+    id: 3,
+  },
+]
 
 const HomeScreen = () => {
 
     const navigation = useNavigation();
 
     return (
-      <SafeAreaView >
+      <SafeAreaView className="flex-1">
 
         <View className="flex-row items-center justify-between px-5">
           <TouchableOpacity>
@@ -28,11 +59,22 @@ const HomeScreen = () => {
           </TouchableOpacity>
         </View>
 
+          <View className="flex-1 -mt-6">
+            <Swiper containerStyle={{ backgroundColor: "transparent" }}
+              cards={DUMMY_DATA}
+              renderCard={(card) => (
+                <View key={card.id} className="relative bg-white h-3/4 rounded-xl" >
+                  <Image className="absolute top-0 h-full w-full rounded-xl"
+                  source={{ uri: card.photoURL }}/>
+                  <Text>{card.firstName}</Text>
+                </View>
+              )}
+            />
+          </View>
+
+
       </SafeAreaView>
     );
 };
-
- /*<Text className="text-rose-700"> I am the Home Screen </Text>
-<Button  title="Go to chat screen" onPress={() => navigation.navigate('Chat')}/>*/
 
 export default HomeScreen
